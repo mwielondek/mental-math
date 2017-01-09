e1028d80bd0addcb280a1469e1d3d205ab879d62
Milosz Wielondek <miloszw@kth.se>
2017-01-09 02:31:45 +0100

    Initial mental math tool stub

--- app/index.js
+++ app/index.js
@@ -3,15 +3,72 @@ var app = document.getElementById('app');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function() {
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+      currentEquation: 0
+    }
+
+    // necessary to access this.state when called from a different context
+    this.solverHandler = this.solverHandler.bind(this);
+  }
+
+  solverHandler(event) {
+    this.setState({
+      score: this.state.score + 1,
+      currentEquation: (this.state.currentEquation + 1) % this.props.equations.length
+    });
+  }
+
+  render() {
+    // TODO add timer
+    const eqs = this.props.equations;
     return (
-      <div> Hello World </div>
+      <div>
+        <Score score={this.state.score} />
+        <Solver equation={eqs[this.state.currentEquation]} scoreHandler={this.solverHandler} />
+      </div>
     );
   }
-});
+};
+Main.defaultProps = {
+  equations: [
+    "8+1",
+    "3+4"
+  ]
+};
+
+const Score = ({score}) => {
+  return <div><p>Your score: {score}</p></div>;
+};
+
+const Solver = ({equation, scoreHandler}) => {
+  const correctAnswer = parseEquation(equation);
+
+  const changeHandler = (event) => {
+    if (event.target.value == correctAnswer) {
+      scoreHandler();  // increase score and get next equation
+      event.persist(); // necessary to avoid the event being released
+      setTimeout(() => {event.target.value = ""}, 220);
+    }
+  };
+
+  return (
+    <div>
+      <p>{equation}</p>
+      <input type="text" onChange={changeHandler} />
+    </div>
+  );
+};
+
+const parseEquation = (equation) => {
+  // TODO
+  return 123;
+}
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Main />,
   app
 );
